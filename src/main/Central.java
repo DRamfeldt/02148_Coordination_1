3e0c0ef834b39b8329c7b983f6da71186759786d
DRamfeldt <DRamfeldt@Gmail.com>
2021-01-09 12:15:32 +0100

    work on supplier

--- src/main/Central.java
+++ src/main/Central.java
@@ -2,4 +2,10 @@ package main;
 import org.jspace.*;
 
 public class Central {
+    Space inventory;
+
+    public Central(SequentialSpace inventory ){
+
+    }
+
 }
--- src/main/Supplier.java
+++ src/main/Supplier.java
@@ -1,5 +1,22 @@
 package main;
 import org.jspace.*;
 
-public class Supplier {
+public class Supplier implements Runnable {
+
+    Space catalogue;
+
+    public Supplier(SequentialSpace catalogue){
+        this.catalogue = catalogue;
+
+    }
+
+    public void request( String item, Integer amount) throws InterruptedException {
+        Object[]  shipment = catalogue.query(new ActualField(item), new FormalField(Integer.class));
+
+    }
+    public void run(){
+        while (true){
+            
+        }
+    }
 }
